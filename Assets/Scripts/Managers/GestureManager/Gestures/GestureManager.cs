6c8b98191f6413973141c78324d712ceaeb1f438
monoAI <46586720+monoai@users.noreply.github.com>
2021-08-13 08:25:42 +0800

    Modified GestureManager.cs
    Made it so that our desired gestures can be attainable, only needed is to actually implement it now.

--- Assets/Scripts/Managers/GestureManager/Gestures/GestureManager.cs
+++ Assets/Scripts/Managers/GestureManager/Gestures/GestureManager.cs
@@ -28,9 +28,11 @@ public class GestureManager : MonoBehaviour
     }
 
     private Touch trackedFinger1;
+    private Touch trackedFinger2;
     private Vector2 startPoint;
     private Vector2 endPoint;
-    private float gestureTime;
+    private float gestureTime1;
+    private float gestureTime2;
 
     // Start is called before the first frame update
     void Start()
@@ -43,44 +45,72 @@ public class GestureManager : MonoBehaviour
     {
         if (Input.touchCount > 0)
         {
+            CheckSingleFingerGestures();
+        }
+        if (Input.touchCount > 1) {
             trackedFinger1 = Input.GetTouch(0);
-            if(trackedFinger1.phase == TouchPhase.Began)
-            {
-                gestureTime = 0;
-                startPoint = trackedFinger1.position;
-            }
-            else if (trackedFinger1.phase == TouchPhase.Ended)
-            {
-                endPoint = trackedFinger1.position;
+            trackedFinger2 = Input.GetTouch(1);
 
-                // Tap Events
-                if (gestureTime <= _tapProperty.tapTime &&
-                    Vector2.Distance(startPoint, endPoint) < (Screen.dpi * _tapProperty.tapMaxDistance))
-                {
-                    FireTapEvent(startPoint);
-                }
+            //yo this is mad ugly, why
+            if(trackedFinger1.phase == TouchPhase.Moved && trackedFinger2.phase == TouchPhase.Began || trackedFinger1.phase == TouchPhase.Stationary && trackedFinger2.phase == TouchPhase.Began) {
+                gestureTime2 = 0;
+                startPoint = trackedFinger2.position;
+            }
+            else if (trackedFinger1.phase == TouchPhase.Moved && trackedFinger2.phase == TouchPhase.Ended) {
+                endPoint = trackedFinger2.position;
 
                 // Swipe Events
-                if (gestureTime <= _swipeProperty.swipeTime &&
+                if (gestureTime2 <= _swipeProperty.swipeTime &&
                     Vector2.Distance(startPoint, endPoint) >=  (Screen.dpi * _swipeProperty.minSwipeDistance) )
                 {
                     FireSwipeEvent();
                 }
             }
-            else
+            else {
+                gestureTime2 += Time.deltaTime;
+            }
+        }
+    }
+
+    private void CheckSingleFingerGestures() {
+        trackedFinger1 = Input.GetTouch(0);
+
+        if(trackedFinger1.phase == TouchPhase.Began)
+        {
+            gestureTime1 = 0;
+            startPoint = trackedFinger1.position;
+        }
+        else if (trackedFinger1.phase == TouchPhase.Ended)
+        {
+            endPoint = trackedFinger1.position;
+
+            // Tap Events
+            if (gestureTime1 <= _tapProperty.tapTime &&
+                Vector2.Distance(startPoint, endPoint) < (Screen.dpi * _tapProperty.tapMaxDistance))
             {
-                gestureTime += Time.deltaTime;
-                if (gestureTime >= _dragProperty.dragBufferTime)
-                {
-                    FireDragEvent();
-                }
+                FireTapEvent(startPoint);
+            }
+
+            // Swipe Events
+            if (gestureTime1 <= _swipeProperty.swipeTime &&
+                Vector2.Distance(startPoint, endPoint) >=  (Screen.dpi * _swipeProperty.minSwipeDistance) )
+            {
+                FireSwipeEvent();
+            }
+        }
+        else
+        {
+            gestureTime1 += Time.deltaTime;
+            if (gestureTime1 >= _dragProperty.dragBufferTime)
+            {
+                FireDragEvent();
             }
         }
     }
 
     private void FireDragEvent()
     {
-        Debug.Log($"Drag: {trackedFinger1.position}");
+        //Debug.Log($"Drag: {trackedFinger1.position}");
 
         Ray r = Camera.main.ScreenPointToRay(trackedFinger1.position);
         RaycastHit hit = new RaycastHit();
